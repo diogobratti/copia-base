5886f9a0a6cacfe5ac6a0731cf33bdddae4c5447
Diogo Bratti <dbratti@gmail.com>
2020-10-04 21:37:09 -0300

    sequelize init

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = require('./app');
 const db = require('./database');
 require('./redis/blacklist')
 
+const { sequelize }  = require('./config');
+
+try {
+     sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
 const port = (process.env.PORT ? process.env.PORT : 3000);
 
 const routes = require('./routes');
