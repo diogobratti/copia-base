b742e358424e1f00751ce80df4a73ec80f965e5f
Diogo Bratti <dbratti@gmail.com>
2020-10-30 23:30:02 -0300

    including json content-type

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -10,6 +10,9 @@ app.use(function(req, res, next) {
 
 const { strategy } = require('./auth');
 
+app.use(
+  bodyParser.json()
+);
 app.use(
   bodyParser.urlencoded({
     extended: true
