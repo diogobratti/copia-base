d044ef2f61057c8eb2c2bacb1fd9656aded76bcb
Diogo Bratti <dbratti@gmail.com>
2020-10-30 23:40:44 -0300

    using cors package

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 const { strategy } = require('./auth');
 
